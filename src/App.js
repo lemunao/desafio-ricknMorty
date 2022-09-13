001dc57640190cabdb4fb546cca4730aa5342732
lemunao <pedroandres1@gmail.com>
2022-09-12 21:44:56 -0300

    added container size

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import { GetImageOfDay } from './Components/Image-day';
 function App() {
   return (
     <div>
-      <section className='container'>
+   
+      <section className='container-xxl'>
         <h1><img src="https://lyricstranslate.com/files/styles/artist/public/Rick_and_Morty_logo.png" alt="" /></h1>
       </section>
       <div>
-
-        <div>
+        <div className='container'>
           <GetImageOfDay />
         </div>
       </div>
